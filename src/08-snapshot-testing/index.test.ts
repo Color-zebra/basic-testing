7ace5c3ad33144d7e1bc315ffed6ddf33ea97bae
Romanenkov Dmitriy <215rd@mail.ru>
2024-06-15 16:06:02 +0300

    feat: solve snapshot-testing task

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,22 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
+  const dataForFirstTest = {
+    value: 1,
+    next: {
+      value: null,
+      next: null,
+    },
+  };
+
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList([1])).toStrictEqual(dataForFirstTest);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList([1, 2])).toMatchSnapshot();
   });
 });
--- src/__mocks__/asd.ts
+++
@@ -1,6 +0,0 @@
-import { AxiosStatic } from 'axios';
-
-const axios = jest.createMockFromModule<AxiosStatic>('axios');
-axios.get = jest.fn().mockReturnValue(Promise.resolve());
-axios.create = jest.fn().mockReturnValue({ get: () => undefined });
-export default axios;
